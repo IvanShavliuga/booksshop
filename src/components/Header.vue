d46d227d864318f59fb10e83f95f5e9cda90145c
Ivan <iva.drakon.nov@gmail.com>
2020-09-17 21:48:40 +0200

    header up

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -29,13 +29,23 @@
             </button>
           </form>
           <div class="header__cart">
-            <i class="header__cart-icon"></i>
+            <i class="header__cart-icon">&nbsp;</i>
             <span class="header__cart-title">
               Your cart
               <button class="header__cart-counter">
                 (2 items)
               </button>
             </span>
+            <span class="header__cart-total">
+              $125.0
+            </span>
+            <button class="header__cart-check">
+              Checkout
+            </button>
+          </div>
+          <div class="header__wishlist">
+            <i class="header__wishlist-icon"></i>
+            <span class="header__wishlist-title">Wish list</span>
           </div>
         </div>
       </div>
@@ -48,6 +58,10 @@
 <style lang="less">
 @import '../assets/css/style.less';
 .header {
+  &__wrapper {
+    .wrapper();
+    height: 140px;
+  }
   &__baseblock {
     background: linear-gradient(#fff,#f7f7f7);
     height: 140px;
@@ -59,6 +73,7 @@
   &__searchpanel {
     display: flex;
     justify-content: space-around;
+    align-items: center;
     &-logo {
       background-image: url('../assets/images/logo.png');
       background-position: center 30px;
@@ -73,7 +88,7 @@
       background-color: @bgform;
       border: 3px solid @bgform;
       padding: 0;
-      margin: 40px 5px;
+      //margin: 40px 5px;
     }
     &-input {
       width: 327px;
@@ -96,7 +111,7 @@
       background-color: @bgform;
       font-size: 23px;
       color: #ffffff;
-      font-family: "Segoe";
+      font-family: @bodyfont;
       position: relative;
       .focus();
     }
@@ -105,7 +120,6 @@
       background-size: 100%;
       width: 18px;
       height: 18px;
-      stroke: white;
       display: block;
       position: absolute;
       z-index: 10;
@@ -114,26 +128,88 @@
     }
   }
   &__cart {
-    margin: 40px 5px;
-    height: 40px;
+    //margin: 20px 1px;
+    width: 195px;
+    height: 90px;
+    padding-right: 1px;
     border-right: 1px solid #ccc;
+    position: relative;
+    &-icon {
+      background-image: url('../assets/images/cart.svg');
+      background-size: 100%;
+      width: 29px;
+      height: 31px;
+      display: block;
+      position: absolute;
+      z-index: 20;
+      top: 11px;
+      left: -13px;
+    }
     &-title {
       font-size: 23px;
-      font-family: "Segoe";
+      line-height: 56px;
+      font-family: @bodyfont;
       color: #6d6d6d;
+      display: inline-block;
+      margin-left: 27px;
+      word-spacing: -1px;
     }
     &-counter {
       font-size: 16px;
       color: #6d6d6d;
       border: none;
+      margin-left: -5px;
+      display: inline-block;
       background-color: transparent;
       .focus();
+    }
+    &-total {
+      font-size: 27px;
+      line-height: 0;
+      padding: 2px;
+      margin-left: -10px;
+      display: inline-block;
+      color: @colform;
+      font-weight: bold;
+      font-family: @bodyfont;
+    }
+    &-check {
+      background-image: linear-gradient(0deg, #242424 0%, #424242 100%);
+      border: 1px solid #232323;
+      font-size: 15px;
+      line-height: 0;
+      display: inline-block;
+      transform: scaleY(1.2);
+      margin-left: 15px;
+      padding: 12px 15px;
+      color: #ffffff;
+      font-family: @bodyfont;
     }
   }
   &__topline {
     background-color: rgb(65,65,65);
     width: 100%;
     height: 30px;
+  }
+  &__wishlist {
+    margin: 5px 15px 5px -35px;
+    padding: 10px;
+    &-title {
+      font-size: 21px;
+      line-height: 26px;
+      color: @colform;
+      font-family: @bodyfont;
+    }
+    &-icon {
+      width: 25px;
+      height: 25px;
+      margin-left: 20px;
+      margin-top: 15px;
+      margin-bottom: 5px;
+      background-image: url('../assets/images/star.svg');
+      background-size: 100%;
+      display: block;
+    }
   }
   &__userpanel {
     display: flex;
@@ -151,7 +227,7 @@
       line-height: 26px;
       color: #ffffff;
       font-weight: 300;
-      font-family: 'Segoe';
+      font-family: @bodyfont;
       text-align: center;
     }
     &-link {
@@ -159,11 +235,6 @@
       text-decoration: none;
       .focus();
     }
-  }
-  &__wrapper {
-    width: 80%;
-    height: 140px;
-    margin: 0 auto;
   }
 }
 </style>
