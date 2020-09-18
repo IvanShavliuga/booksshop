e8949261220eb015d80f03fa3594cc82bc542cc5
Ivan <iva.drakon.nov@gmail.com>
2020-09-18 15:32:02 +0200

    aside | category list, left menu

--- src/components/Catlist.vue
+++ src/components/Catlist.vue
@@ -1,40 +1,41 @@
 <template>
   <aside class="aside">
+    <h2 class="aside__header">Categories</h2>
     <nav class="aside__nav">
       <ul>
-        <li class="aside__nav-item">All</li>
-        <li class="aside__nav-item">Fiction & Literature</li>
-        <ul>
-          <li class="aside__nav-item">Children</li>
-          <li class="aside__nav-item">Science</li>
-          <li class="aside__nav-item">Fantasy</li>
-          <li class="aside__nav-item">Mystery</li>
-          <li class="aside__nav-item">Romance</li>
-          <li class="aside__nav-item">Horror</li>
-          <li class="aside__nav-item">Poetry</li>
-          <li class="aside__nav-item">Literature</li>
-          <li class="aside__nav-item">Crime</li>
+        <li class="aside__nav-item itemactive"><a class="aside__nav-link" href="#cat"> All</a></li>
+        <li class="aside__nav-item"><a class="aside__nav-link" href="#cat"> Fiction & Literature</a></li>
+        <ul class="aside__nav-list">
+          <li class="aside__nav-cat"><a class="aside__nav-link" href="#cat"> Children</a></li>
+          <li class="aside__nav-cat"><a class="aside__nav-link" href="#cat"> Science</a></li>
+          <li class="aside__nav-cat"><a class="aside__nav-link" href="#cat"> Fantasy</a></li>
+          <li class="aside__nav-cat"><a class="aside__nav-link" href="#cat"> Mystery</a></li>
+          <li class="aside__nav-cat"><a class="aside__nav-link" href="#cat"> Romance</a></li>
+          <li class="aside__nav-cat"><a class="aside__nav-link" href="#cat"> Horror</a></li>
+          <li class="aside__nav-cat"><a class="aside__nav-link" href="#cat"> Poetry</a></li>
+          <li class="aside__nav-cat"><a class="aside__nav-link" href="#cat"> Literature</a></li>
+          <li class="aside__nav-cat"><a class="aside__nav-link" href="#cat"> Crime</a></li>
         </ul>
-        <li class="aside__nav-item">Non - Fiction</li>
-        <ul>
-          <li class="aside__nav-item">Comic</li>
-          <li class="aside__nav-item">Cook</li>
-          <li class="aside__nav-item">Psychology</li>
-          <li class="aside__nav-item">Medical</li>
-          <li class="aside__nav-item">Art</li>
-          <li class="aside__nav-item">Photography</li>
-          <li class="aside__nav-item">Law</li>
-          <li class="aside__nav-item">History</li>
-          <li class="aside__nav-item">Business</li>
-          <li class="aside__nav-item">Computers</li>
+        <li class="aside__nav-item"><a class="aside__nav-link" href="#cat"> Non - Fiction</a></li>
+        <ul class="aside__nav-list">
+          <li class="aside__nav-cat"><a class="aside__nav-link" href="#cat"> Comic</a></li>
+          <li class="aside__nav-cat"><a class="aside__nav-link" href="#cat"> Cook</a></li>
+          <li class="aside__nav-cat"><a class="aside__nav-link" href="#cat"> Psychology</a></li>
+          <li class="aside__nav-cat"><a class="aside__nav-link" href="#cat"> Medical</a></li>
+          <li class="aside__nav-cat"><a class="aside__nav-link" href="#cat"> Art</a></li>
+          <li class="aside__nav-cat"><a class="aside__nav-link" href="#cat"> Photography</a></li>
+          <li class="aside__nav-cat"><a class="aside__nav-link" href="#cat"> Law</a></li>
+          <li class="aside__nav-cat"><a class="aside__nav-link" href="#cat"> History</a></li>
+          <li class="aside__nav-cat"><a class="aside__nav-link" href="#cat"> Business</a></li>
+          <li class="aside__nav-cat"><a class="aside__nav-link" href="#cat"> Computers</a></li>
         </ul>
-        <li class="aside__nav-item">Other</li>
-        <ul>
-          <li class="aside__nav-item">Baby</li>
-          <li class="aside__nav-item">Sex</li>
-          <li class="aside__nav-item">Travel</li>
-          <li class="aside__nav-item">Science</li>
-          <li class="aside__nav-item">Sport`s</li>
+        <li class="aside__nav-item"><a class="aside__nav-link" href="#cat"> Other</a></li>
+        <ul class="aside__nav-list">
+          <li class="aside__nav-cat"><a class="aside__nav-link" href="#cat"> Baby</a></li>
+          <li class="aside__nav-cat"><a class="aside__nav-link" href="#cat"> Sex</a></li>
+          <li class="aside__nav-cat"><a class="aside__nav-link" href="#cat"> Travel</a></li>
+          <li class="aside__nav-cat"><a class="aside__nav-link" href="#cat"> Science</a></li>
+          <li class="aside__nav-cat"><a class="aside__nav-link" href="#cat"> Sport`s</a></li>
         </ul>
       </ul>
     </nav>
@@ -54,13 +55,50 @@ export default {
   font-weight: bold;
 }
 .aside {
+  width: 17%;
+  margin: 15px;
+  padding: 0;
+  border: 1px solid @bordercolor;
+  &__header {
+    padding: 0 20px;
+    margin-bottom: 0;
+    font-size: 24px;
+    line-height: 30px;
+    color: #6b6969;
+    font-weight: 300;
+    font-family: @bodyfont;
+  }
   &__nav {
+    margin-top: 0;
+    &-list {
+      padding: 0 20px;
+    }
+    &-cat {
+      border-bottom: 1px solid @bordercolor;
+      font-size: 15px;
+      line-height: 27px;
+      color: #555454;
+      font-family: @bodyfont;
+    }
     &-item {
-      border-bottom:1px solid #dddddd;
-      padding-top:5px;
-      padding-bottom: 5px;
-      padding-left: 10px;
+      padding: 5px 20px;
+      font-size: 17px;
+      line-height: 32px;
+      color: #525252;
+      font-weight: bold;
+      font-family: @bodyfont;
       .hover();
+      &:first-child {
+        text-transform: uppercase;
+      }
+    }
+    &-link {
+      font-size: 15px;
+      line-height: 26px;
+      color: #5b5b5b;
+      font-family: @bodyfont;
+      margin: 0;
+      text-decoration: none;
     }
   }
 }
