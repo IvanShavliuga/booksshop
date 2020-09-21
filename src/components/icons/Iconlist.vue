24808e214c7ed63eee68b05ad177c9f7f5fc9580
Ivan <iva.drakon.nov@gmail.com>
2020-09-21 16:49:48 +0200

    icons | created svg icons component

--- src/components/icons/Iconlist.vue
+++ src/components/icons/Iconlist.vue
@@ -1,49 +1,59 @@
 <template>
 <ul class="icons">
   <li class="icons__item">
-    <ib class="icons__image"
-      icon-name="codepen"
-      width="64"
-      height="64"
-      icon-color="#5434ad">
-      <codepen/>
-    </ib>
+    <a class="icons__link" href="https://www.codepen.io/ivanshavliuga">
+      <ib class="icons__image"
+        icon-name="codepen"
+        width="64"
+        height="64"
+        icon-color="#1223cb">
+        <codepen/>
+      </ib>
+    </a>
   </li>
   <li class="icons__item">
-    <ib class="icons__image"
-      icon-name="telegram"
-      width="64"
-      height="64"
-      icon-color="#5434ad">
-      <telegram/>
-    </ib>
+    <a class="icons__link" href="https://t.me/ivanshavliuga">
+      <ib class="icons__image"
+        icon-name="telegram"
+        width="64"
+        height="64"
+        icon-color="#1223cb">
+        <telegram/>
+      </ib>
+    </a>
   </li>
   <li class="icons__item">
-    <ib class="icons__image"
-      icon-name="github"
-      width="64"
-      height="64"
-      icon-color="#5434ad">
-      <github/>
-    </ib>
+    <a class="icons__link" href="https://www.github.com/IvanShavliuga">
+      <ib class="icons__image"
+        icon-name="github"
+        width="64"
+        height="64"
+        icon-color="#1223cb">
+        <github/>
+      </ib>
+    </a>
   </li>
   <li class="icons__item">
-    <ib class="icons__image"
-      icon-name="linkedin"
-      width="64"
-      height="64"
-      icon-color="#5434ad">
-      <linkedin/>
-    </ib>
+    <a class="icons__link" href="https://www.linkedin.com/in/%D0%B8%D0%B2%D0%B0%D0%BD-%D0%B8%D0%B2%D0%B0%D0%BD%D0%BE%D0%B2-%D1%88%D0%B0%D0%B2%D0%BB%D1%8E%D0%B3%D0%B0-778540153/">
+      <ib class="icons__image"
+        icon-name="linkedin"
+        width="64"
+        height="64"
+        icon-color="#1223cb">
+        <linkedin/>
+      </ib>
+    </a>
   </li>
   <li class="icons__item">
-    <ib class="icons__image"
-      icon-name="vk"
-      width="64"
-      height="64"
-      icon-color="#5434ad">
-      <vk/>
-    </ib>
+    <a class="icons__link" href="https://www.vk.com/isshavluga">
+      <ib class="icons__image"
+        icon-name="vk"
+        width="64"
+        height="64"
+        icon-color="#1223cb">
+        <vk/>
+      </ib>
+    </a>
   </li>
 </ul>
 </template>
@@ -76,7 +86,7 @@ export default {
     margin: 0;
     border: 1px solid @bordercolor;
     transform: scale(0.6);
-    background: linear-gradient(#f7f7f7,#a7a7a7);
+    background: linear-gradient(#f7f7f7,#b7b7b7);
   }
   &__image {
     margin: 0;
