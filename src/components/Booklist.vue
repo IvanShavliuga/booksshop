3af1a9e5602da841a1875b2aed7d0027391ee2e1
Ivan <iva.drakon.nov@gmail.com>
2020-09-24 11:10:42 +0200

    booklist | books list component layout

---
+++ src/components/Booklist.vue
@@ -0,0 +1,23 @@
+<template>
+<div class="list">
+  <Book v-for="(i,k) in [1,2,3,4,5,6,7,8,9,1,2,3,4,5,6,7,8,9]" :key="i+k"/>
+</div>
+</template>
+<style scoped lang="less">
+@import '../assets/css/style.less';
+
+.list {
+  border: 1px solid @bordercolor;
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-between;
+}
+</style>
+<script>
+import Book from '@/components/Book.vue'
+export default {
+  components: {
+    Book
+  }
+}
+</script>
