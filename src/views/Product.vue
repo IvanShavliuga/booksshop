0ffa8fc828227b921e4e25dd32f92b26fd71043e
Ivan <iva.drakon.nov@gmail.com>
2020-09-24 19:35:42 +0200

    product | new page, footer and header, hello include

---
+++ src/views/Product.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="product">
+    <Hello/>
+    <Header/>
+    <Footer/>
+  </div>
+</template>
+
+<script>
+// @ is an alias hto /src
+import Header from '@/components/Header.vue'
+import Hello from '@/components/HelloWorld.vue'
+import Footer from '@/components/Footer.vue'
+
+export default {
+  name: 'Home',
+  components: {
+    Header,
+    Hello,
+    Footer
+  }
+}
+</script>
+<style scoped lang="less">
+@import '../assets/css/style.less';
+
+.product {
+  &__wrapper {
+    .wrapper();
+  }
+  &__flex {
+    display: flex;
+  }
+  &__block {
+    width: 70vw;
+  }
+}
+</style>
