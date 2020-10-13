251106aca18e0529523733bdd117365f8c053bd4
Ivan <iva.drakon.nov@gmail.com>
2020-10-13 19:06:11 +0200

    nuxt version

---
+++ .nuxt/components/index.js
@@ -0,0 +1,41 @@
+export { default as Banner } from '../..\\components\\Banner.vue'
+export { default as Book } from '../..\\components\\Book.vue'
+export { default as Bookbanner } from '../..\\components\\Bookbanner.vue'
+export { default as Booklist } from '../..\\components\\Booklist.vue'
+export { default as Catlist } from '../..\\components\\Catlist.vue'
+export { default as Commentbox } from '../..\\components\\Commentbox.vue'
+export { default as Footer } from '../..\\components\\Footer.vue'
+export { default as Header } from '../..\\components\\Header.vue'
+export { default as HelloWorld } from '../..\\components\\HelloWorld.vue'
+export { default as Path } from '../..\\components\\Path.vue'
+export { default as Promo } from '../..\\components\\Promo.vue'
+export { default as Tabspanel } from '../..\\components\\Tabspanel.vue'
+export { default as Codepen } from '../..\\components\\icons\\codepen.vue'
+export { default as Github } from '../..\\components\\icons\\github.vue'
+export { default as IconBase } from '../..\\components\\icons\\IconBase.vue'
+export { default as Iconlist } from '../..\\components\\icons\\Iconlist.vue'
+export { default as Linkedin } from '../..\\components\\icons\\linkedin.vue'
+export { default as Portfolio } from '../..\\components\\icons\\portfolio.vue'
+export { default as Telegram } from '../..\\components\\icons\\telegram.vue'
+export { default as Vk } from '../..\\components\\icons\\vk.vue'
+
+export const LazyBanner = import('../..\\components\\Banner.vue' /* webpackChunkName: "components_Banner" */).then(c => c.default || c)
+export const LazyBook = import('../..\\components\\Book.vue' /* webpackChunkName: "components_Book" */).then(c => c.default || c)
+export const LazyBookbanner = import('../..\\components\\Bookbanner.vue' /* webpackChunkName: "components_Bookbanner" */).then(c => c.default || c)
+export const LazyBooklist = import('../..\\components\\Booklist.vue' /* webpackChunkName: "components_Booklist" */).then(c => c.default || c)
+export const LazyCatlist = import('../..\\components\\Catlist.vue' /* webpackChunkName: "components_Catlist" */).then(c => c.default || c)
+export const LazyCommentbox = import('../..\\components\\Commentbox.vue' /* webpackChunkName: "components_Commentbox" */).then(c => c.default || c)
+export const LazyFooter = import('../..\\components\\Footer.vue' /* webpackChunkName: "components_Footer" */).then(c => c.default || c)
+export const LazyHeader = import('../..\\components\\Header.vue' /* webpackChunkName: "components_Header" */).then(c => c.default || c)
+export const LazyHelloWorld = import('../..\\components\\HelloWorld.vue' /* webpackChunkName: "components_HelloWorld" */).then(c => c.default || c)
+export const LazyPath = import('../..\\components\\Path.vue' /* webpackChunkName: "components_Path" */).then(c => c.default || c)
+export const LazyPromo = import('../..\\components\\Promo.vue' /* webpackChunkName: "components_Promo" */).then(c => c.default || c)
+export const LazyTabspanel = import('../..\\components\\Tabspanel.vue' /* webpackChunkName: "components_Tabspanel" */).then(c => c.default || c)
+export const LazyCodepen = import('../..\\components\\icons\\codepen.vue' /* webpackChunkName: "components_icons/codepen" */).then(c => c.default || c)
+export const LazyGithub = import('../..\\components\\icons\\github.vue' /* webpackChunkName: "components_icons/github" */).then(c => c.default || c)
+export const LazyIconBase = import('../..\\components\\icons\\IconBase.vue' /* webpackChunkName: "components_icons/IconBase" */).then(c => c.default || c)
+export const LazyIconlist = import('../..\\components\\icons\\Iconlist.vue' /* webpackChunkName: "components_icons/Iconlist" */).then(c => c.default || c)
+export const LazyLinkedin = import('../..\\components\\icons\\linkedin.vue' /* webpackChunkName: "components_icons/linkedin" */).then(c => c.default || c)
+export const LazyPortfolio = import('../..\\components\\icons\\portfolio.vue' /* webpackChunkName: "components_icons/portfolio" */).then(c => c.default || c)
+export const LazyTelegram = import('../..\\components\\icons\\telegram.vue' /* webpackChunkName: "components_icons/telegram" */).then(c => c.default || c)
+export const LazyVk = import('../..\\components\\icons\\vk.vue' /* webpackChunkName: "components_icons/vk" */).then(c => c.default || c)
---
+++ nuxt.config.js
@@ -0,0 +1,38 @@
+export default {
+  // Global page headers (https://go.nuxtjs.dev/config-head)
+  head: {
+    title: 'MusicClub',
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { hid: 'description', name: 'description', content: '' }
+    ],
+    link: [
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }/*,
+      { rel: 'stylesheet', type: 'text/CSS', href='https://fonts.googleapis.com/css2?family=Noto+Sans:ital,wght@0,400;0,700;1,400;1,700&display=swap'}*/
+    ]
+  },
+
+  // Global CSS (https://go.nuxtjs.dev/config-css)
+  css: [
+  ],
+
+  // Plugins to run before rendering page (https://go.nuxtjs.dev/config-plugins)
+  plugins: [
+  ],
+
+  // Auto import components (https://go.nuxtjs.dev/config-components)
+  components: true,
+
+  // Modules for dev and build (recommended) (https://go.nuxtjs.dev/config-modules)
+  buildModules: [
+  ],
+
+  // Modules (https://go.nuxtjs.dev/config-modules)
+  modules: [
+  ],
+
+  // Build Configuration (https://go.nuxtjs.dev/config-build)
+  build: {
+  }
+}
