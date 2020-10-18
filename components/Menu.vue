9afc9b58748e46d64095dbb34dd69267ff9173c8
Ivan <iva.drakon.nov@gmail.com>
2020-10-18 14:31:15 +0200

    catlist, menu | burger tablet and mobile design

--- components/Menu.vue
+++ components/Menu.vue
@@ -1,6 +1,24 @@
 <template>
 <nav class="menu">
   <div class="menu__wrapper">
+    <div class="menu__collapse">
+      <a class="menu__collapse-title">Menu</a>
+      <a class="menu__collapse-burger">
+        <svg xmlns="http://www.w3.org/2000/svg"
+          width="32"
+          height="32"
+          viewBox="0 0 32 32"
+          role="presentation"
+        >
+          <title lang="en">menu</title>
+          <g stroke="#76ae89" fill="transparent">
+            <line x1="2" y1="14" x2="30" y2="14"/>
+            <line x1="2" y1="20" x2="30" y2="20"/>
+            <line x1="2" y1="26" x2="30" y2="26"/>
+          </g>
+        </svg>
+      </a>
+    </div>
     <ul class="menu__list">
       <li class="menu__list-item itemactive">
         <a class="menu__list-link" href="cm">Computers</a></li>
@@ -37,6 +55,29 @@
   &__wrapper {
     .wrapper();
   }
+  &__collapse {
+    width: 100%;
+    height: 40px;
+    @media screen and (min-width: 1061px) {
+      display: none;
+    }
+    &-title {
+      float: left;
+      font-size: 28px;
+      line-height: 42px;
+      text-transform: uppercase;
+      color: #ccc;
+      @media screen and (max-width: 910px) {
+        margin-left: 15px;
+      }
+    }
+    &-burger {
+      float: right;
+      @media screen and (max-width: 910px) {
+        margin-right: 15px;
+      }
+    }
+  }
   &__list {
     display: flex;
     justify-content: center;
@@ -45,6 +86,7 @@
     @media screen and (max-width: 1060px) {
       flex-direction: column;
       height: auto;
+      display: none;
     }
     &-item {
       list-style: none;
